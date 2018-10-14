1fbcb4187689f14ab0fad0dc2507c1a3360ccf8e
flyc <yayayahahahaooii@gmail.com>
2018-10-14 22:35:52 +0800

    use index.mjs replace babel.

---
+++ index.mjs
@@ -0,0 +1,20 @@
+// please make sure your nodejs version is higher than 10.4.0
+
+import axios from 'axios';
+import fs from 'fs'
+import cheerio from 'cheerio';
+
+(async () => {
+    var array = await axios({
+        method: 'get',
+        url: 'https://www.google.com.tw'
+    }).then((res) => {
+        console.log('res');
+        return [res]
+    }).catch((error) => {
+        console.log('error');
+        return [error]
+    });
+
+    console.log(array);
+})();
