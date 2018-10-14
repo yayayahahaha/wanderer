25b395a07b8154f4ec6c8879020d62c52e978cb5
flyc <yayayahahahaooii@gmail.com>
2018-10-15 00:18:04 +0800

    add comments and request header

--- index.mjs
+++ index.mjs
@@ -2,19 +2,37 @@
 
 import axios from 'axios';
 import fs from 'fs'
-import cheerio from 'cheerio';
+import cheerio from 'cheerio'; //var $ = cheerio.load(res.data);
 
-(async () => {
-    var array = await axios({
-        method: 'get',
-        url: 'https://www.google.com.tw'
-    }).then((res) => {
-        console.log('res');
-        return [res]
-    }).catch((error) => {
-        console.log('error');
-        return [error]
-    });
+var url = 'https://www.pixiv.net/search.php?s_mode=s_tag&word=',
+    keyword = 'kill la kill';
 
-    console.log('done');
-})();
+url = `${url}${keyword}`;
+
+// getSearchPage(url);
+getSingleImage('https://i.pximg.net/img-master/img/2015/07/11/15/56/58/51359343_p0_master1200.jpg');
+
+// url
+// https://i.pximg.net/img-master/img/2014/07/13/20/39/04/44690099_p0_master1200.jpg
+// referer
+// https://www.pixiv.net/member_illust.php?mode=medium&illust_id=44690099
+
+async function getSearchPage(url) {
+    var [data, error] = await axios.get(url)
+        .then(({
+            data
+        }) => {
+            return [data, null];
+        })
+        .catch((err) => {
+            return [null, err];
+        });
+
+    if (error) {
+        console.log('發生錯誤了');
+        console.log(error.request);
+        return;
+    }
+
+    console.log(data);
+}
