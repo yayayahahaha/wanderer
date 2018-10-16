540bde8b00e495cf2f93db524f308746ae6d35e1
flyc <yayayahahahaooii@gmail.com>
2018-10-17 00:41:42 +0800

    prune cookies, remove result file

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ import request from 'request';
 
 // var keyword = 'kill la kill',
 var keyword = 'darling in the franxx',
-    page = 1,
+    page = 100,
     totalPages = null,
     totalCount = null,
     likedLevel = 50;
@@ -17,33 +17,7 @@ var getSearchHeader = function() {
             // accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             // 'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5',
-            cookie:
-                'first_visit_datetime_pc=2018-10-17+01%3A20%3A22;' +
-                'p_ab_id=8;' +
-                'p_ab_id_2=8;' +
-                'p_ab_d_id=2064289869;' +
-                'yuid_b=OGSQEHA;' +
-                '__utma=235335808.1823058345.1539706823.1539706823.1539706823.1;' +
-                '__utmc=235335808;' +
-                '__utmz=235335808.1539706823.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none);' +
-                '__utmt=1;' +
-                '_ga=GA1.2.1823058345.1539706823;' +
-                '_gid=GA1.2.259623744.1539706836;' +
-                'PHPSESSID=35210002_3f5f551db1e08d29d3c4dd07f6469308;' +
-                'device_token=d5baf71a554db04c07a4481eff5386ab;' +
-                'c_type=25;' +
-                'a_type=0;' +
-                'b_type=0;' +
-                'privacy_policy_agreement=0;' +
-                'login_ever=yes;' +
-                '__utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^6=user_id=35210002=1^9=p_ab_id=8=1^10=p_ab_id_2=8=1^11=lang=zh_tw=1;' +
-                'user_language=zh_tw;' +
-                'module_orders_mypage=%5B%7B%22name%22%3A%22sketch_live%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22tag_follow%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22recommended_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22fanbox%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22user_events%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D;' +
-                'is_sensei_service_user=1;' +
-                '_gat_UA-1830249-138=1;' +
-                'ki_t=1539707282245%3B1539707282245%3B1539707282245%3B1%3B1;' +
-                'ki_r=;' +
-                '__utmb=235335808.12.10.1539706823'
+            cookie: 'PHPSESSID=35210002_3f5f551db1e08d29d3c4dd07f6469308;'
             // referer: 'https://www.pixiv.net/search.php?s_mode=s_tag&word=darling%20in%20the%20franxx',
             // 'upgrade-insecure-requests': 1
         };
@@ -98,6 +72,9 @@ async function firstSearch(url) {
     totalPages = Math.ceil(totalCount / 40);
     console.log(`搜尋結束, 總筆數有 ${totalCount} 件, 共 ${totalPages} 頁`); // !! 與實際頁面數不符，沒有登入好像只有 10 頁
     console.log(`開始從中挑選出愛心數大於 ${likedLevel} 顆的連結..`);
+
+    // 用來測試實際取到的結果
+    // fs.writeFileSync('result', data);
 }
 
 // TODO:
