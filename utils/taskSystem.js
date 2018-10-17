8c08eca71cc866430a88df628e3dceaaf120f202
flyc <yayayahahahaooii@gmail.com>
2018-10-18 00:49:57 +0800

    remove taskSystem.js update taskSystem.mjs

--- utils/taskSystem.js
+++ utils/taskSystem.js
@@ -1,72 +1,45 @@
 function TaskSystem(jobsArray = [], resultArray = [], taskNumber = 5, callback = Function.prototype) {
-    var self = this;
-    self.jobsArray = jobsArray;
-    self.resultArray = resultArray;
-    self.callback = callback;
-    self.taskNumber = taskNumber;
+    this.jobsArray = jobsArray;
+    this.resultArray = resultArray;
+    this.callback = callback;
+    this.taskNumber = taskNumber;
 
-    self.taskListObject = {};
-    self.sequenceCounter = 0;
-    self.totalJobsNumber = self.jobsArray.length;
+    this.workingTasksNumber = 0;
+    this.sequenceCounter = 0;
+    this.totalJobsNumber = this.jobsArray.length;
 
-    for (var i = 0; i < jobsArray.length; i++) {
-        jobsArray[i]
-    }
+    this.doJobs = async function(resolve) {
+        var job = null,
+            jobReault = null,
+            lastOne = false;
 
-    function _Task(sequenceNumber) {
-        this.sequence = sequenceNumber;
-        this.doPromise = async () => {
-            var promise = null,
-                promiseReault = null,
-                lastOne = false;
+        if (this.jobsArray.length === 0) {
+            console.log('工作結束!');
+            this.workingTasksNumber--;
 
-            if (this.__proto__.jobsArray.length === 0) {
-                console.log('工作完成!');
-                delete this.__proto__.taskListObject[this.sequence];
-
-                if (Object.keys(this.__proto__.taskListObject).length === 0) {
-                    this.__proto__.callback(this.__proto__.returnResult);
-                }
-                return; // 這裡是return
-            }
-
-            // 從jobsArray 裡取出promise function
-            promise = this.__proto__.jobsArray.splice(0, 1)[0];
-
-            // 執行或直接賦值
-            promiseReault = typeof promise === 'function' ? await promise() : promise;
-
-            // 推進結果裡
-            this.__proto__.returnResult.push(promiseReault);
-
-            console.log(this.__proto__.jobsArray);
-
-            if (lastOne) {
-                this.__proto__.callback(this.__proto__.returnResult);
-            }
-
-            // 再來一次
-            this.doPromise();
+            console.log(`還剩下${this.workingTasksNumber}個task 還在執行`);
+            return;
         }
 
-        // 首次直接執行
-        this.doPromise();
+        job = this.jobsArray.splice(0, 1)[0];
+
+        jobReault = typeof job === 'function' ? await job() : job;
+
+        this.resultArray.push(jobReault);
+
+        this.doJobs(resolve);
     }
+
+    this._doPromise() {
+        return new Promise((resolve, reject) => {
+            for (var i = 0; i < jobsArray.length; i++) {
+                this.workingTasksNumber++;
+                this.doJobs(resolve);
+            }
+        })
+    }
+    this._doPromise();
 }
-
-TaskSystem.prototype.init = function(jobsArray = [], returnResult = [], taskNumber = 8, callback = Function.prototype) {
-
-    TaskSystem.prototype.jobsArray = jobsArray.slice();
-    TaskSystem.prototype.returnResult = returnResult;
-    TaskSystem.prototype.callback = callback;
-
-    for (var i = 0; i < taskNumber; i++) {
-        TaskSystem.prototype.sequenceCounter++;
-        var sequence = `Task-System-${TaskSystem.prototype.sequenceCounter}`; // 製作流水號
-
-        TaskSystem.prototype.taskListObject[sequence] = new TaskSystem(sequence);
-    }
-};
 
 export {
     TaskSystem
--- utils/taskSystem.mjs
+++ utils/taskSystem.mjs
@@ -1,61 +1,45 @@
-function TaskSystem(sequenceNumber) {
-    this.sequence = sequenceNumber;
-    this.doPromise = async () => {
-        var promise = null,
-            promiseReault = null,
+function TaskSystem(jobsArray = [], resultArray = [], taskNumber = 5, callback = Function.prototype) {
+    this.jobsArray = jobsArray;
+    this.resultArray = resultArray;
+    this.callback = callback;
+    this.taskNumber = taskNumber;
+
+    this.workingTasksNumber = 0;
+    this.sequenceCounter = 0;
+    this.totalJobsNumber = this.jobsArray.length;
+
+    this.doJobs = async function(resolve) {
+        var job = null,
+            jobReault = null,
             lastOne = false;
 
-        if (this.__proto__.sourceArray.length === 0) {
-            console.log('工作完成!');
-            delete this.__proto__.taskListObject[this.sequence];
+        if (this.jobsArray.length === 0) {
+            console.log('工作結束!');
+            this.workingTasksNumber--;
 
-            if (Object.keys(this.__proto__.taskListObject).length === 0) {
-                this.__proto__.callback(this.__proto__.returnResult);
+            console.log(`還剩下${this.workingTasksNumber}個task 還在執行`);
+            return;
+        }
+
+        job = this.jobsArray.splice(0, 1)[0];
+
+        jobReault = typeof job === 'function' ? await job() : job;
+
+        this.resultArray.push(jobReault);
+
+        this.doJobs(resolve);
+    }
+
+    this._doPromise = () => {
+        return new Promise((resolve, reject) => {
+            for (var i = 0; i < jobsArray.length; i++) {
+                this.workingTasksNumber++;
+                this.doJobs(resolve);
             }
-            return; // 這裡是return
-        }
-
-        // 從sourceArray 裡取出promise function
-        promise = this.__proto__.sourceArray.splice(0, 1)[0];
-
-        // 執行或直接賦值
-        promiseReault = typeof promise === 'function' ? await promise() : promise;
-
-        // 推進結果裡
-        this.__proto__.returnResult.push(promiseReault);
-
-        console.log(this.__proto__.sourceArray);
-
-        if (lastOne) {
-            this.__proto__.callback(this.__proto__.returnResult);
-        }
-
-        // 再來一次
-        this.doPromise();
+        })
     }
-
-    // 首次直接執行
-    this.doPromise();
+    this._doPromise();
 }
-TaskSystem.prototype.sourceArray = [];
-TaskSystem.prototype.returnResult = [];
-TaskSystem.prototype.taskListObject = {};
-TaskSystem.prototype.callback = Function.prototype;
-TaskSystem.prototype.sequenceCounter = 0;
-
-TaskSystem.prototype.init = function(sourceArray = [], returnResult = [], taskNumber = 8, callback = Function.prototype) {
-
-    TaskSystem.prototype.sourceArray = sourceArray.slice();
-    TaskSystem.prototype.returnResult = returnResult;
-    TaskSystem.prototype.callback = callback;
-
-    for (var i = 0; i < taskNumber; i++) {
-        TaskSystem.prototype.sequenceCounter++;
-        var sequence = `Task-System-${TaskSystem.prototype.sequenceCounter}`; // 製作流水號
-
-        TaskSystem.prototype.taskListObject[sequence] = new TaskSystem(sequence);
-    }
-};
 
 export {
     TaskSystem
