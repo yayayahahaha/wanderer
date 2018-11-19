77677e3f1456be0a8d4a37b7150d145c6d4c0be7
flyc <yayayahahahaooii@gmail.com>
2018-11-19 23:12:41 +0800

    update sessionId

--- index.mjs
+++ index.mjs
@@ -23,7 +23,7 @@ import {
 // TODO
 // SESSID 的部分可以嘗試打post api 傳遞帳密後直接取得之類的
 // 或是取得多組SESSID 後放進array 做輪詢減少單一帳號的loading 之類的
-var currentSESSID = '35210002_15804c6e5996fa0146b13f18472b802b';
+var currentSESSID = '35210002_11042cace126e0ec64e65f52291bb859';
 
 var keyword = '',
     page = 1,
